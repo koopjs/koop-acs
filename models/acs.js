aee45dd74180fa1248c898055273f19aa5748eec
chelm <christopher.helm@gmail.com>
2014-08-12 19:20:05 -0600

    not joining data

--- models/acs.js
+++ models/acs.js
@@ -1,22 +1,11 @@
-var request = require('request'),
-  async = require('async'),
-  pg = require('pg');
+var request = require('request');
 
-var config = require('./config');
 
 // American Community Survery API model
 // handles all calls to ACS and interacts with the koop cache
 
 var ACS = function(){
 
-  // connect to postgis for geom lookups 
-  this.client = new pg.Client(config.db);
-  this.client.connect(function(err) {
-    if(err) {
-      console.error('could not connect to postgres', err);
-    }
-  });
-  
   // generates a key used to lookup data in the cache
   this.genKey = function(params){
     return [params['year'], params['state'], params['for'], params['variable']].join('::');
@@ -27,24 +16,6 @@ var ACS = function(){
   this.find = function( params, options, callback ){
     var self = this;
 var k = 0;
-    var q = async.queue(function (task, cb) {
-      console.log(k++, geojson.features.length);
-      self.client.query(task.query, function(err, result) {
-        task.feature.geometry = JSON.parse( result.rows[0].geom );
-        geojson.features.push(task.feature);
-        cb( ); 
-      });
-    }, 4);
-  
-    q.drain = function(){
-      // insert data 
-      console.log('DONE', geojson.features.length);  
-      Cache.insert( type, key, geojson, 0, function( err, success){
-        if ( success ) {
-          callback( null, geojson );
-        }
-      });
-    };
 
     // check for the needed params 
     // we could add some validation to the terms used here (in a new method _validate)
@@ -76,28 +47,21 @@ var k = 0;
                 row.forEach(function(col,j){
                   feature.properties[headers[j]] = col;
                 });
-                  if ( feature.properties.tract ){
-                    query = "select st_asgeojson(geom) as geom from tracts where tractce = '"+feature.properties.tract+"' AND statefp = '"+feature.properties.state+"' AND countyfp = '"+feature.properties.county+"'";
-                    q.push({query: query, feature: feature}, function(f){
-                      //console.log('wtf'); 
-                      //geojson.features.push( f );
-                    });
-                    //console.log('look up tract ', col);
-                  }
-                  //geojson.features.push( feature );
+                geojson.features.push( feature );
               }
             });
-            //console.log('wtf');
-            //callback(null, geojson)
+            Cache.insert( type, key, geojson, 0, function( err, success){
+              if ( success ) {
+                callback( null, geojson );
+              }
+            });
             
           } catch(e){
-            console.log(e);
             callback(res.body, null);
           }  
         });
       } else {
         // We have data already, send it back
-        console.log('good');
         callback( null, entry );
       }
     });
