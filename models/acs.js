9de3adfc323f91d0878cc01ad3482b4616987b7f
chelm <christopher.helm@gmail.com>
2015-04-24 08:59:30 -0600

    searching on all datasets

--- models/acs.js
+++ models/acs.js
@@ -141,7 +141,7 @@ var ACS = function( koop ){
   // searches across all ACS data in the cache
   acs.search = function( options, callback ){
     // drops the item from the cache
-    options.type = 'acs';
+    //options.type = 'acs';
     koop.Cache.db.select('all', options, function(err, res){
       callback(null, res);
     });
